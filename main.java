bc2a8120a47635183d85177d022b839e5511aea4
dinabakhtiari <142736449+dinabakhtiari@users.noreply.github.com>
2026-01-15 08:43:41 +0000

    add two numbers

--- main.java
+++ main.java
@@ -1,5 +1,11 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+
+        int num1 = 20;
+        int num2 = 35;
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
     }
 }
