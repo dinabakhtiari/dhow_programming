fc8ab2461749c27f1549ecca82ad704542c860b6
dinabakhtiari <142736449+dinabakhtiari@users.noreply.github.com>
2026-01-15 08:46:00 +0000

    sum: Add two numbers and print the result

---
+++ sum.java
@@ -0,0 +1,12 @@
+public class Sum {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 40;
+        int num2 = 20;
+        int result = add(num1, num2);
+        System.out.println("Sum: " + result);
+    }
+}
