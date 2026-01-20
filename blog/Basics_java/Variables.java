633aee497626a5e8419d28a8f8c66d90c4602b0f
dinabakhtiari <142736449+dinabakhtiari@users.noreply.github.com>
2026-01-20 21:40:41 +0000

    Variable types

--- blog/Basics_java/Variables.java
+++ blog/Basics_java/Variables.java
@@ -1,10 +1,39 @@
+import java.util.Date;
+
 public class Variables{
 
     public static void main(String[] args) {
         //System.out.println("Hi My name is Dina!");
-        int age = 24;
-        System.out.print(age);
-        
+        // int age = 24;
+        // System.out.print(age);
+
+        // byte --> 1 byte -128 -- 127
+        // short --> 2 byte -32k -- 32k
+        // int --> 4 byte -2b -- 2b
+        // long --> 8 byte
+
+        // float --> 4 byte
+        // double --> 8 byte
+
+        // char
+        // boolean 1 byte --> True or False
+        // string
+
+        byte age = 24;
+        long viewCount = 6123456789L;
+        float price = 18.99F;
+        boolean darkMode = false;
+
+        Date now = new Date();
+        now.getTime();
+
+        System.out.println(now);
+        String my_name = "Dina" + " Bakhtiari";
+        System.out.println(my_name.startsWith("Di"));
+        System.out.println(my_name.length());
+        System.out.println(my_name.indexOf("n"));
+        System.out.println(my_name.replace("Dina", "Diaro"));
+        System.out.println("-----------------------------");
 
     }
 }
