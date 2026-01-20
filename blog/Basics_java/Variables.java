9845e0ce9994c55db516b88c6ace84f9ab58685d
dinabakhtiari <142736449+dinabakhtiari@users.noreply.github.com>
2026-01-20 20:03:42 +0000

    first commit for Variables

---
+++ blog/Basics_java/Variables.java
@@ -0,0 +1,10 @@
+public class Variables{
+
+    public static void main(String[] args) {
+        //System.out.println("Hi My name is Dina!");
+        int age = 24;
+        System.out.print(age);
+        
+
+    }
+}
