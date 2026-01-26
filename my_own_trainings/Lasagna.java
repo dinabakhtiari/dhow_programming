08ef1a778f4d7b5cc8b686f3954e90e1637cd60c
Dina Bakhtiari <dinaba701@gmail.com>
2026-01-26 19:06:08 +0100

    add the last method for calculating total time and complete the main method to test all methods.

--- my_own_trainings/Lasagna.java
+++ my_own_trainings/Lasagna.java
@@ -12,11 +12,15 @@ public class Lasagna{
     public int preparationTimeInMinutes(int layers) {
         return layers * 2;
     }
-    
+
+    public int totalTimeInMinutes(int layers, int actualMinutes){
+        return  preparationTimeInMinutes(layers) + actualMinutes ;
+    }
     public static void main(String[] args) {
         Lasagna lasagna = new Lasagna();
         System.out.println(lasagna.expectedMinutesInOven());
         System.out.println(lasagna.remainingMinutesInOven(30));
         System.out.println(lasagna.preparationTimeInMinutes(2));
+        System.out.println(lasagna.totalTimeInMinutes(3,20));
     }
 }
